a4693619cfc10803d94c1b48a5a25cba638c7709
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-15 11:27:55 +0100

    created song model

--- models/Song.js
+++ models/Song.js
@@ -1,7 +1,12 @@
+const { DataTypes } = require('sequelize');
 const {Sequelize, sequelize} = require('../db');
 
 // TODO - define the Song model
-let Song;
+const Song = sequelize.define("Song",{
+    title: DataTypes.STRING,
+    year: DataTypes.NUMBER,
+    length: DataTypes.NUMBER
+})
 
 module.exports = {
     Song
