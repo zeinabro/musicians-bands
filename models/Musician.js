d87f4b8018e690df9118ec7f00b11a65f8f767e2
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-15 11:26:57 +0100

    created musician model

--- models/Musician.js
+++ models/Musician.js
@@ -1,7 +1,11 @@
+const { DataTypes } = require('sequelize');
 const {Sequelize, sequelize} = require('../db');
 
 // TODO - define the Musician model
-let Musician;
+const Musician = sequelize.define("Musician",{
+    name: DataTypes.STRING,
+    instrument: DataTypes.STRING
+})
 
 module.exports = {
     Musician
