61b73ac959726418913cf82dbaf8ff6321fa6a96
Zeinab Roumieh <zeinabroumieh@outlook.com>
2024-05-15 11:32:19 +0100

    debugging band model

--- models/Band.js
+++ models/Band.js
@@ -1,8 +1,6 @@
 const { DataTypes } = require('sequelize');
 const {Sequelize, sequelize} = require('../db');
 
-class Band extends Model {};
-
 const Band = sequelize.define('Band',{
     name: DataTypes.STRING,
     genre: DataTypes.STRING
